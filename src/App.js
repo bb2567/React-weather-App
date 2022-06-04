c038c63c6420b6e5754135e5e60fe9cae3cd54f3
mark <marksong0976@gmail.com>
2022-06-04 14:29:29 +0800

    add normalize、emotion/core、emotion/styled

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import './App.css';
+import styled from "@emotion/styled";
+
+
+const Container = styled.div`
+  background-color:#ededed;
+  height:100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+const WeatherCard = styled.div`
+  position:relative;
+  min-width:360px;
+  box-shadow: 0 1px 3px 0 #999999;
+  background-color: #f9f9f9;
+  padding:30px15px;
+  text-align:center
+`
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hi</h1>
-    </div>
+    <Container>
+      <WeatherCard>
+        <h1>Taiwan Weather</h1>
+      </WeatherCard>
+    </Container>
   );
 }
 
