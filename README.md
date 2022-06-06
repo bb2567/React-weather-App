f3352a0ff17e0c774089d9e00de37c42b6bb6e29
mark <marksong0976@gmail.com>
2022-06-06 22:28:30 +0800

    使用 <ThemeProvider >  控制 child components

--- README.md
+++ README.md
@@ -3,6 +3,10 @@
 
 
 ## 使用套件
-CSS reset[normalize](https://www.npmjs.com/package/normalize.css?activeTab=versions) 
-CSS [emotion/core](https://www.npmjs.com/package/@emotion/core)  
-CSS [emotion/styled](https://www.npmjs.com/package/normalize.css?activeTab=versions)  
+reset[normalize](https://www.npmjs.com/package/normalize.css?activeTab=versions) 
+使各預覽器CSS標準化
+
+[emotion/core](https://www.npmjs.com/package/@emotion/core)  
+[emotion/styled](https://www.npmjs.com/package/normalize.css?activeTab=versions)  
+[emotion/react](https://emotion.sh/docs/theming) 
+使用 ThemeProvider 元件控管子元件Styled
--- src/App.js
+++ src/App.js
@@ -1,78 +1,115 @@
 import styled from "@emotion/styled";
+import { ThemeProvider } from '@emotion/react'
 import { ReactComponent as DayCloudyIcon } from "./images/day-cloudy.svg";
 import { ReactComponent as RainIcon } from "./images/rain.svg";
 import { ReactComponent as AirFlowIcon } from './images/airFlow.svg'
 import { ReactComponent as RefreshIcon } from './images/refresh.svg'
 
+
+const theme = {
+  light: {
+    backgroundColor: '#ededed',
+    foregroundColor: '#f9f9f9',
+    boxShadow: '0 1px 3px 0 #999999',
+    titleColor: '#212121',
+    temperatureColor: '#757575',
+    textColor: '#828282',
+  },
+  dark: {
+    backgroundColor: '#1F2022',
+    foregroundColor: '#121416',
+    boxShadow:
+      '0 1px 4px 0 rgba(12, 12, 13, 0.2), 0 0 0 1px rgba(0, 0, 0, 0.15)',
+    titleColor: '#ffffff',
+    temperatureColor: '#dddddd',
+    textColor: '#cccccc',
+  },
+};
+
+
+
 const Container = styled.div`
-  background-color:#ededed;
-  height:100%;
+  background-color: ${({ theme }) => theme.backgroundColor};
+  height: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
-`
+`;
+
 const WeatherCard = styled.div`
-  position:relative;
-  min-width:360px;
-  box-shadow: 0 1px 3px 0 #999999;
-  background-color: #f9f9f9;
-  box-sizing:border-box;
-  padding:30px 15px;
+  position: relative;
+  min-width: 360px;
+  box-shadow: ${({ theme }) => theme.boxShadow};
+  background-color: ${({ theme }) => theme.foregroundColor};
+  box-sizing: border-box;
+  padding: 30px 15px;
   border-radius:3%
-`
+`;
+
 const Location = styled.div`
-  font-size:28px;
-  color:#212121;
-  margin-bottom:20px;
-`
+  font-size: 28px;
+  color: ${({ theme }) => theme.titleColor};
+  margin-bottom: 20px;
+`;
+
 const Description = styled.div`
   font-size: 16px;
-  color: #828282;
+  color: ${({ theme }) => theme.textColor};
   margin-bottom: 30px;
-`
-const CurrentWeather = styled.div`
+`;
 
+const CurrentWeather = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
   margin-bottom: 30px;
-`
+`;
+
 const Temperature = styled.div`
-  color: #757575;
+  color: ${({ theme }) => theme.temperatureColor};
   font-size: 96px;
   font-weight: 300;
   display: flex;
-`
+`;
+
 const Celsius = styled.div`
-  font-weight: normal;  
+  font-weight: normal;
   font-size: 42px;
-`
+`;
 
 const AirFlow = styled.div`
   display: flex;
   align-items: center;
   font-size: 16x;
   font-weight: 300;
-  color: #828282;
+  color: ${({ theme }) => theme.textColor};
   margin-bottom: 20px;
+
   svg {
     width: 25px;
     height: auto;
     margin-right: 30px;
   }
-`
+`;
+
 const Rain = styled.div`
   display: flex;
   align-items: center;
   font-size: 16x;
   font-weight: 300;
-  color: #828282;
+  color: ${({ theme }) => theme.textColor};
+
   svg {
     width: 25px;
     height: auto;
     margin-right: 30px;
   }
-`
+`;
+
+const DayCloudy = styled(DayCloudyIcon)`
+  flex-basis: 30%;
+`;
+
 const Refresh = styled.div`
   position: absolute;
   right: 15px;
@@ -80,39 +117,38 @@ const Refresh = styled.div`
   font-size: 12px;
   display: inline-flex;
   align-items: flex-end;
-  color: #828282;
+  color: ${({ theme }) => theme.textColor};
+
   svg {
     margin-left: 10px;
     width: 15px;
     height: 15px;
     cursor: pointer;
   }
-`
-
+`;
 
 
 function App() {
   return (
-    <Container>
-      <WeatherCard>
-        <Location>台北市</Location>
-        <Description>多雲時晴</Description>
-        <CurrentWeather>
-          <Temperature>23<Celsius>°C</Celsius></Temperature>
-          <DayCloudyIcon />
-
-
-
-        </CurrentWeather>
-        <AirFlow>
-          <AirFlowIcon />23 m/h
-        </AirFlow>
-        <Rain>
-          <RainIcon />48%
-        </Rain>
-        <Refresh>最後觀測資料：上午12:00 <RefreshIcon /></Refresh>
-      </WeatherCard>
-    </Container>
+    <ThemeProvider theme={theme.light}>
+      <Container >
+        <WeatherCard>
+          <Location>台北市</Location>
+          <Description>多雲時晴</Description>
+          <CurrentWeather>
+            <Temperature>23<Celsius>°C</Celsius></Temperature>
+            <DayCloudyIcon />
+          </CurrentWeather>
+          <AirFlow>
+            <AirFlowIcon />23 m/h
+          </AirFlow>
+          <Rain>
+            <RainIcon />48%
+          </Rain>
+          <Refresh>最後觀測資料：上午12:00 <RefreshIcon /></Refresh>
+        </WeatherCard>
+      </Container>
+    </ThemeProvider>
   );
 }
 
